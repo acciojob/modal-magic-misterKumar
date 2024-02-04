caf8dbc4a8741858420de433d4a511cdbe88453a
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-04 21:21:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 //your JS code here. If required.
-const modal = document.getElementById('modal')
-const open = document.getElementById('openModal')
-function handleClick(){
-	modal.style.display = "block"
-}
-function handleClose(){
-	modal.style.display = "none"
-}
-// window.addEventListener("click", function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// });
+document.getElementById("openModalBtn").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "block";
+});
 
-window.addEventListener('click',(e)=>{
-	if(e.target !== open && !modal.contains(e.target)){
-	handleClose()
-	}
-})
+document.getElementById("closeModalBtn").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == document.getElementById("modal")) {
+    document.getElementById("modal").style.display = "none";
+  }
+});
